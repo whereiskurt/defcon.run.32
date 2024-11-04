ee52b42ca77a8342c9751ca3dac21f6610d5385b
KPH <whereiskurt@gmail.com>
2024-11-03 21:22:53 -0500

    -Strava refresh retry

--- workspace/apps/web/db/src/lib/util/heatmap.ts
+++ workspace/apps/web/db/src/lib/util/heatmap.ts
@@ -1,10 +1,15 @@
 import { UserEntity } from "../db";
 import { DC32_UNIX_END, DC32_UNIX_START, fetchActivities, StravaActivity, updateStravaToken } from "../strava";
 
+
+// function sleep(ms: number) {
+//   return new Promise(resolve => setTimeout(resolve, ms));
+// }
+
 export async function ProcessHeatMap(unixNow: number = Math.floor(Date.now() / 1000), beforeUnix: number = DC32_UNIX_END, afterUnix: number = DC32_UNIX_START) {
   try {
     const users = await UserEntity.scan();
-
+    // let c = 0;
     for (const user of users.Items || []) {
       const { email } = user;
 
@@ -14,6 +19,9 @@ export async function ProcessHeatMap(unixNow: number = Math.floor(Date.now() / 1
       if (!(email === 'whereiskurt@gmail.com')) continue;
 
       strava_account = await updateStravaToken({ email, strava_account, unixNow });
+      // c++;
+      // if (c > 5) break;
+      // await sleep(3000);
 
       const resp = await fetchActivities({ strava_account, beforeUnix, afterUnix });
       if (resp['error']) {
